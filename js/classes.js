2513e50f3f22eb3345ef3b48cbc3c037217e9f32
Beau Marwaha <beaumarwaha@gmail.com>
2018-10-26 01:57:57 -0400

    Cleared out old code and added a simple main menu
    Cleared out the old unused code from the project and added a simple main menu to the game.

--- js/classes.js
+++ js/classes.js
@@ -1,30 +1,17 @@
-import {getRandomUnitVector} from './utilities.js';
-export {createCircleSprites,createSquareSprites,createImageSprites,createLineSprites};
+import {} from './utilities.js';
+export {createCircleSprite,createSquareSprite,createImageSprite};
 
 class sprite{
-		constructor(x,y,fwd,speed){
+		constructor(x,y){
 			this.x = x;
 			this.y = y;
-			this.fwd = fwd;
-			this.speed = speed;
-		}
-		//  methods
-		move(){
-				this.x += this.fwd.x * this.speed;
-				this.y += this.fwd.y * this.speed;
-		}
-		reflectX(){
-			this.fwd.x *= -1;
-		}
-		reflectY(){
-			this.fwd.y *= -1;
 		}
 }
 
 class circleSprite extends sprite{
-	constructor(rect,radius,color)
+	constructor(x,y,radius,color)
 	{
-		super(Math.random() * rect.width + rect.left, Math.random() * rect.height + rect.top, getRandomUnitVector(), 2);
+		super(x,y,radius,color);
 		this.radius = radius;
 		this.color = color;
 	}
@@ -40,23 +27,23 @@ class circleSprite extends sprite{
 }
 
 class squareSprite extends sprite{
-	constructor(rect,width,height,color){
-		super(Math.random() * rect.width + rect.left, Math.random() * rect.height + rect.top, getRandomUnitVector(), 2);
+	constructor(x,y,width,height,color){
+		super(x, y);
 		this.width = width;
 		this.height = height;
 		this.color = color;
 	}
 	draw(ctx){ 
 		ctx.save();
-		ctx.fillStyle = this.color;
-		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.strokeStyle = this.color;
+		ctx.strokeRect(this.x, this.y, this.width, this.height);
 		ctx.restore();
 	}
 }
 
 class imageSprite extends sprite{
-	constructor(rect,width,height,image){
-		super(Math.random() * rect.width + rect.left, Math.random() * rect.height + rect.top, getRandomUnitVector(), 2);
+	constructor(x,y,width,height,image){
+		super(x,y);
 		this.width = width;
 		this.height = height;
 		this.image = image;
@@ -68,59 +55,16 @@ class imageSprite extends sprite{
 	}
 }
 
-class lineSprite extends sprite{
-	constructor(rect,length,color){
-		super(Math.random() * rect.width + rect.left, Math.random() * rect.height + rect.top, getRandomUnitVector(), 2);
-		this.length = length;
-		this.height = 5;
-		this.color = color;
-	}
-	draw(ctx){
-		ctx.save();
-		ctx.beginPath();
-		ctx.moveTo(this.x, this.y);
-		ctx.lineTo(this.x + this.length, this.y);
-		ctx.strokeStyle = this.color;
-		ctx.lineWidth = this.height;
-		ctx.stroke();
-		ctx.restore();
-	}
+function createCircleSprite(x=0, y=0, color="Blue", radius=5){
+	return new circleSprite(x,y,radius,color); 
 }
 
-function createCircleSprites(num=20, rect={left:0,top:0,width:300,height:300}, color="Blue", radius=5){
-	let sprites = [];
-	for(let i=0;i<num;i++){
-		sprites.push(new circleSprite(rect,radius,color));
-	}
-	
-	return sprites; 
+function createSquareSprite(x=0, y=0, color="Blue", width=10, height=10){
+	return new squareSprite(x,y,width,height,color); 
 }
 
-function createSquareSprites(num=20, rect={left:0,top:0,width:300,height:300}, color="Blue", width=10, height=10){
-	let sprites = [];
-	for(let i=0;i<num;i++){
-		sprites.push(new squareSprite(rect,width,height,color));
-	}
-	
-	return sprites; 
-}
-
-function createImageSprites(num=20, rect={left:0,top:0,width:300,height:300}, width=30, height=30, url="images/Sean.png"){
-	let sprites = [];
+function createImageSprite(x, y, width=30, height=30, url="images/Sean.png"){
 	let image = new Image();
 	image.src = url; 
-	for(let i=0;i<num;i++){
-		sprites.push(new imageSprite(rect,width,height,image));
-	}
-	
-	return sprites; 
-}
-
-function createLineSprites(num=20, rect={left:0,top:0,width:300,height:300}, color="Blue", length=10){
-	let sprites = [];
-	for(let i=0;i<num;i++){
-		sprites.push(new lineSprite(rect,length,color));
-	}
-	
-	return sprites; 
+	return new imageSprite(x,y,width,height,image); 
 }
--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 import {init} from './main.js';
 // 1) this script a good place to load fonts, images, sounds and other resources
+
 // 2) start up app
 init();
